064afa023b78ae77501f94cf0993a44675b550d5
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-04-18 20:01:09 +0530

    Added solution - LeetHub

---
+++ K-Ary Tree - GFG/kary-tree.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long karyTree(int k, int m) {
+        // code here
+        long long ans=1;
+        
+        for(int i=1;i<=m;i++){
+            ans=(ans*k)%1000000007;
+        }
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k,m;
+        
+        cin>>k>>m;
+
+        Solution ob;
+        cout << ob.karyTree(k,m) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends

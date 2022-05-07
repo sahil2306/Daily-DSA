c075787d32665a3c91db641af61a57ea06cabc99
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-05-07 22:30:50 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,116 @@
+// { Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to merge two sorted linked list.
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    // code here
+    Node *head=new Node(-1),*tail;
+    Node *curr1=head1,*curr2=head2;
+    tail=head;
+    
+    while(curr1!=NULL and curr2!=NULL){
+        if(curr1->data < curr2->data){
+            tail->next = curr1;
+            curr1=curr1->next;
+            tail=tail->next;
+        }
+        else{
+            tail->next=curr2;
+            curr2=curr2->next;
+            tail=tail->next;
+        }
+    }
+    
+    if(curr1){
+        tail->next = curr1;
+        curr1=curr1->next;
+        tail=tail->next;
+    }
+    else{
+        tail->next=curr2;
+            curr2=curr2->next;
+            tail=tail->next;
+    }
+    
+    return head->next;
+}  

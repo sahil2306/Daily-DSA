e2e877d30590a0367630c3800dcf0288c427ea5b
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-05-21 13:17:51 +0530

    Time: 89 ms (75.20%), Space: 14.5 MB (54.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp((amount+1),1e9);
+        vector<int> dp((amount+1),10001);
         dp[0]=0;
         
         for(int i=1;i<=amount;i++){
@@ -12,7 +12,7 @@ public:
             }
         }
 
-        return dp[amount]!=1e9?dp[amount]:-1;
+        return dp[amount]!=10001?dp[amount]:-1;
         
     }
 };

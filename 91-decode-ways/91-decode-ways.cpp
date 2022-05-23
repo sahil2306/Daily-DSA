f943de7ebe145e2e095626999f51396b1cb0955b
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-05-24 00:50:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (72.32%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    int helper(string s,int idx,vector<int>& dp){
-        if(idx==s.size()) return dp[idx]=1;
-        if(dp[idx]!=-1) return dp[idx];
-
-        char ch = s[idx];
-        if(ch=='0')return dp[idx]=0;
-        
-        int count = helper(s,idx+1,dp);
-
-        if(idx<s.size()-1){
-            char ch2 = s[idx+1];
-            int no = (ch-'0')*10 + (ch2-'0');
-            if(no<27){
-                count+=helper(s,idx+2,dp);
+    int tabu(string s,vector<int>& dp){
+        for(int idx=s.size();idx>=0;idx--){
+            if (idx == s.length()) {
+                dp[idx] = 1;
+                continue;
             }
+            char ch = s[idx];
+            if(ch=='0'){
+                dp[idx]=0;
+                continue;
+            }
+            int count = dp[idx+1];
+
+            if(idx<(s.size()-1)){
+                char ch2 = s[idx+1];
+                int no = (ch-'0')*10 + (ch2-'0');
+                if(no<27){
+                    count += dp[idx+2];//count+=helper(s,idx+2,dp);
+                }
+            }
+            dp[idx] = count;
         }
 
-        return dp[idx]=count;
+        return dp[0];
     }
     int numDecodings(string s) {
         vector<int> dp(s.size()+1,-1);
         int ans=0;
-        return helper(s,0,dp);
+        return tabu(s,dp);
     }
 };

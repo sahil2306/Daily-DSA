d304bdbaaaddb202e8f9e2f3ee0c351efb05c2dc
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-05-24 20:28:06 +0530

    Time: 3 ms (54.62%), Space: 5.9 MB (99.97%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int tabu(string s,vector<int>& dp){
-        for(int idx=s.size();idx>=0;idx--){
-            if (idx == s.length()) {
-                dp[idx] = 1;
-                continue;
-            }
+    int numDecodings(string s) {
+        int a=1,b=0;
+        for(int idx=s.size()-1;idx>=0;idx--){
             char ch = s[idx];
-            if(ch=='0'){
-                dp[idx]=0;
-                continue;
-            }
-            int count = dp[idx+1];
-
-            if(idx<(s.size()-1)){
+            int c=0;
+            if(ch!='0'){
+                c+=a;
+                if(idx<(s.size()-1)){
                 char ch2 = s[idx+1];
                 int no = (ch-'0')*10 + (ch2-'0');
                 if(no<27){
-                    count += dp[idx+2];//count+=helper(s,idx+2,dp);
+                    c+=b;
                 }
             }
-            dp[idx] = count;
+            }
+            
+            b=a;
+            a=c;
         }
-
-        return dp[0];
-    }
-    int numDecodings(string s) {
-        vector<int> dp(s.size()+1,-1);
-        int ans=0;
-        return tabu(s,dp);
+        return a;
     }
 };

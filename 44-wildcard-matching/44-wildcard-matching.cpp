43a146cafc54c7747b275894d795d1d004ca7efe
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-06-01 22:53:41 +0530

    Time: 54 ms (70.88%), Space: 25.4 MB (35.24%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -22,34 +22,34 @@ public:
         return s;
     }
     
-    bool isMatch_memo(string &s1, string &s2, vector<vector<int>> &dp, int i, int j){
-        if(i==0 or j==0){
-            if(i==0 and j==0)return dp[i][j]=1;
-            if(j==1 and s2[j-1]=='*')return dp[i][j]=1;
-            return dp[i][j]=0;
-        }
-        
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s1[i-1]==s2[j-1] or s2[j-1]=='?'){
-            return dp[i][j]=isMatch_memo(s1,s2,dp,i-1,j-1);
-        }
-        else if(s2[j-1]=='*'){
-            bool res=0;
-            res = res || isMatch_memo(s1,s2,dp,i-1,j);
-            res = res || isMatch_memo(s1,s2,dp,i,j-1); 
-            return dp[i][j]=res;
-        }
-        return dp[i][j]=0;
-    }
-    
     bool isMatch(string s1, string s2) {
-        
         s2 = removeStars(s2);
         int n1=s1.size(),n2=s2.size();
         vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-        return isMatch_memo(s1,s2,dp,n1,n2);
+        
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0 or j==0){
+                    if(i==0 and j==0){dp[i][j]=1;continue;}
+                    else if(j==1 and s2[j-1]=='*'){dp[i][j]=1;continue;}
+                    else{dp[i][j]=0;continue;}
+                }
+                if(s1[i-1]==s2[j-1] or s2[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                    continue;
+                }
+                else if(s2[j-1]=='*'){
+                    bool res=0;
+                    res = res || dp[i-1][j];
+                    res = res || dp[i][j-1]; 
+                    dp[i][j]=res;
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        return dp[n1][n2];
     }
 };

1e8b282ef5be4ce5fd75272444a2107e9ca1a37a
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2022-06-01 19:43:05 +0530

    Time: 23 ms (41.50%), Space: 9 MB (56.66%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int minDistance_memo(string &s1, string &s2, vector<vector<int>> &dp, int i, int j){
-        if(i==0 or j==0){
-            return dp[i][j]=(j==0?i:j);
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        //udpdate
-        int upd = minDistance_memo(s1,s2,dp,i-1,j-1);
-        if(s1[i-1]==s2[j-1]){
-            return dp[i][j] = upd;
-        }
-        //insert:
-        int ins = minDistance_memo(s1,s2,dp,i,j-1);
-        //delete
-        int del = minDistance_memo(s1,s2,dp,i-1,j);
-        dp[i][j] = min(ins,min(upd,del))+1;
-        
-        return dp[i][j];
-    }
+    int minDistance(string s1, string s2) {
+        int n1 = s1.size(),n2=s2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
 
-
-    int minDistance(string word1, string word2) {
-        int n1 = word1.size(),n2=word2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-        return minDistance_memo(word1,word2,dp,n1,n2);
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0 or j==0){
+                    dp[i][j]=(j==0?i:j);
+                    continue;
+                }
+                int upd = dp[i-1][j-1];
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = upd;
+                    continue;
+                }
+                //insert:
+                int ins = dp[i][j-1];
+                //delete
+                int del = dp[i-1][j];
+                dp[i][j] = min(ins,min(upd,del))+1;
+            }
+        }
+        return dp[n1][n2];
     }
 };

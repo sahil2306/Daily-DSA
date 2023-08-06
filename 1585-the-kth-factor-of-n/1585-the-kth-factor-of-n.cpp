ddaf57502b7a17bafff6bd0751a0d256ce47d1b9
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2023-08-05 18:34:44 -0700

    Time: 3 ms (37.27%), Space: 6.1 MB (8.85%) - LeetHub

---
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        priority_queue<int> pq;
+        for(int i=1;i<=sqrt(n);i++){
+            if(i*i==n){
+                pq.push(i);
+            }
+            else if(n%i==0){
+                pq.push(i);
+                pq.push(n/i);
+            }
+        }
+        
+
+        int sz = pq.size();
+        if(k>sz){
+            return -1;
+        }
+        for(int i=0;i<(sz-k);i++){
+            cout<<pq.top();
+            pq.pop();
+        }
+        return pq.top();
+    }
+};

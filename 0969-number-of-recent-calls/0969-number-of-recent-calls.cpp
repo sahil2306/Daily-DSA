c55f53bbc166c6acfb267bf7ba2780cac805b810
Sahil Morankar <56814542+sahil2306@users.noreply.github.com>
2023-08-08 14:31:09 -0700

    Time: 133 ms (85.7%), Space: 57.4 MB (43.91%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
@@ -1,20 +1,15 @@
 class RecentCounter {
-    int min_idx;
 public:
-    vector<int> v;
+    queue<int> q;
     RecentCounter() {
-        v={};
-        min_idx=0;
     }
     
     int ping(int t) {
-        v.push_back(t);
-        while((t-v[min_idx])>3000){
-            min_idx++;
+        q.push(t);
+        while(q.front()< t - 3000){
+            q.pop();
         }
-
-        return v.size()-min_idx;
-
+        return q.size();
     }
 };
 
